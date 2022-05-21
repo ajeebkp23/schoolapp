8ba11c372b30272a8f7cbadbef91ff0368761a80
Ajeeb.K.P <ajeebkp23@gmail.com>
2022-05-21 20:52:18 +0530

    Initial Commit with some deletions

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<script>
+export default {
+  data() {
+    return {
+      message: 'simple one'
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <input v-model="message" placeholder="edit" />
+      <p>Message is {{ message }}</p>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Class List</h1>
+    <span>This page displays a list of classes</span>
+  </main>
+</template>
